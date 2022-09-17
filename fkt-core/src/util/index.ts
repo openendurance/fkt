da47c3c40393e6ad863da4c9976b2109078295ad
Kristopher Cargile <kris@kriscargile.com>
2022-09-17 08:57:42 -0600

    drop old index.ts files

--- fkt-core/src/util/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * File: /fkt/fkt-core/src/util/index.ts
- * Created Date: Thursday, 3rd September 2020 2:13:03 pm
- * Author: Kristopher Cargile <k@openendurance.org>
- *
- * Copyright (c) 2022 Algorythmic, LLC
- */
-
-// export { DateTimeUtil } from "#fkt-core/util/DateTimeUtil";
