67eb84b4371ef676f40ffb0152102f725d3ecec7
Kristopher Cargile <kris@kriscargile.com>
2022-09-21 12:24:36 -0600

    drop file prologs

--- fkt-core/src/IFktObject.ts
+++ fkt-core/src/IFktObject.ts
@@ -1,11 +1,3 @@
-/*
- * Copyright 2020 Algorythmic, LLC
- *
- * Use of this source code is governed by an MIT-style
- * license that can be found in the LICENSE file or at
- * https://opensource.org/licenses/MIT.
- */
-
 import { ILocation } from "./geo/ILocation";
 
 /**
--- fkt-core/src/IRun.ts
+++ fkt-core/src/IRun.ts
@@ -1,11 +1,3 @@
-/*
- * Copyright 2020 Algorythmic, LLC
- *
- * Use of this source code is governed by an MIT-style
- * license that can be found in the LICENSE file or at
- * https://opensource.org/licenses/MIT.
- */
-
 import { IClub } from "./IClub";
 import { IFktObject } from "./IFktObject";
 import { IOrganizer } from "./IOrganizer";
