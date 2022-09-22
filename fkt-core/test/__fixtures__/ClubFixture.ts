06b41e4e401c88a0cd728090153aa3e30b1c69ec
Kristopher Cargile <kris@kriscargile.com>
2022-09-22 17:19:13 -0600

    refactor fixtures

---
+++ fkt-core/test/__fixtures__/ClubFixture.ts
@@ -0,0 +1,24 @@
+import { v4 } from "uuid";
+
+export const create = () => {
+	return {
+		address: "421 E Main St, Buena Vista, CO 81211",
+		coordinates: { latitude: 38.84310337541561, longitude: -106.12797954471321 },
+		createdAt: Date.now(),
+		email: "porter@grouprunfinder.com",
+		facebook: "http://localhost.com",
+		id: v4(),
+		instagram: "http://localhost.com",
+		name: "Porter's Running Club",
+		phone: "404-555-1212",
+		twitter: "http://localhost.com",
+		website: "http://localhost.com",
+	};
+}
+
+export const empty = () => {
+	return {
+		name: "Porter's Running Club",
+		createdAt: Date.now(),
+	};
+}
